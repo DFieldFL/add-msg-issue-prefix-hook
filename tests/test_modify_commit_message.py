77c7a8b3e73fbdd3d3dc6f317945064ec5bdd5b8
Brandon Groves <brandon.groves@pm.me>
2025-03-06 22:12:42 -0500

    formatting

--- tests/test_modify_commit_message.py
+++ tests/test_modify_commit_message.py
@@ -3,13 +3,13 @@ import re
 
 
 def test_modify_commit_message():
-    modified_message = modify_commit_message('tests commit message', 'my_prefix',  re.compile('^'))
+    modified_message = modify_commit_message('tests commit message', 'my_prefix', re.compile('^'))
     assert modified_message == 'my_prefix tests commit message'
 
 def test_modify_commit_message_strip():
-    modified_message = modify_commit_message('tests commit message', ' my_prefix ',  re.compile('^'))
+    modified_message = modify_commit_message('tests commit message', ' my_prefix ', re.compile('^'))
     assert modified_message == 'my_prefix tests commit message'
 
 def test_modify_commit_message_after():
-    modified_message = modify_commit_message('tests commit message', 'my_prefix',  re.compile('^tests'))
+    modified_message = modify_commit_message('tests commit message', 'my_prefix', re.compile('^tests'))
     assert modified_message == 'tests my_prefix commit message'
